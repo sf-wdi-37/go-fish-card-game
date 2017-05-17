3a8f539fa566c67cd3764842c2f4a3d919452498
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2017-05-17 15:46:35 -0700

    Update 1_playing_card_spec.rb

--- spec/1_playing_card_spec.rb
+++ spec/1_playing_card_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe PlayingCard, :type => :model do
     describe "#initialize" do
       it "accepts a single argument (MAKE THIS ARGUMENT A HASH!)" do
         expect{ PlayingCard.new }.to raise_error(ArgumentError)
-        expect{ PlayingCard.new(Hash.new) }.not_to raise_error
+        expect{ PlayingCard.new({rank:'2',suit:'H'}) }.not_to raise_error
         expect { @card }.not_to raise_error
       end
       it "accesses the hash for keys :rank and :suit (and assigns them to an instance variable!)" do
